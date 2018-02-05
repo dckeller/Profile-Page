03aaa1ba5203d6fb5b21e27acfcdb008d008f0a5
David Keller <dckeller1@gmail.com>
2018-02-05 16:12:45 -0700

    All elements complete from instruction

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  get 'contact/new'
+  
+  resources :contacts, only: [:new, :create]
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
