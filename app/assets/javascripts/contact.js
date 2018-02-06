126d12746581fc9790cf907ef1adcc4266778380
David Keller <dckeller1@gmail.com>
2018-02-05 19:05:05 -0700

    Added in JS

---
+++ app/assets/javascripts/contact.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+	
+	$("#mailer").on("click", function(){
+		$(this).fadeOut('slow').addClass("hidden")
+		$(".form").fadeIn('slow').removeClass("hidden")
+	})
+
+	$('.button').on("submit", "form" function() {
+		$(".form").fadeOut('slow').addClass("hidden")
+		$("#mailer").fadeIn('slow').removeClass("hidden")
+	})
+});
